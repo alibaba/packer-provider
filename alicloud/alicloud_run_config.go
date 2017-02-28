c6f94989be06925f9c9986bb598bfbb40aa792c0
zhuzhih2017 <dongxiao.zzh@alibaba-inc.com>
2017-02-28 10:47:17 +0800

    Update the image ID and update some properties

--- alicloud/alicloud_run_config.go
+++ alicloud/alicloud_run_config.go
@@ -7,7 +7,6 @@ import (
 	"github.com/mitchellh/packer/helper/communicator"
 	"github.com/mitchellh/packer/template/interpolate"
 	"os"
-	"time"
 )
 
 const (
@@ -17,27 +16,26 @@ const (
 )
 
 type RunConfig struct {
-	AssociatePublicIpAddress bool          `mapstructure:"associate_public_ip_address"`
-	ZoneId                   string        `mapstructure:"zone_id"`
-	IOOptimized              bool          `mapstructure:"io_optimized"`
-	InstanceType             string        `mapstructure:"instance_type"`
-	Description              string        `mapstructure:"description"`
-	AlicloudSourceImage      string        `mapstructure:"source_image"`
-	ForceStopInstance        bool          `mapstructure:"force_stop_instance"`
-	SecurityGroupId          string        `mapstructure:"security_group_id"`
-	SecurityGroupName        string        `mapstructure:"security_group_name"`
-	UserData                 string        `mapstructure:"user_data"`
-	UserDataFile             string        `mapstructure:"user_data_file"`
-	VpcId                    string        `mapstructure:"vpc_id"`
-	VpcName                  string        `mapstructure:"vpc_name"`
-	CidrBlock                string        `mapstructure:"vpc_cidr_block"`
-	VSwitchId                string        `mapstructure:"vswitch_id"`
-	VSwitchName              string        `mapstructure:"vswitch_id"`
-	InstanceName             string        `mapstructure:"instance_name"`
-	InternetChargeType       string        `mapstructure:"internet_charge_type"`
-	InternetMaxBandwidthOut  int           `mapstructure:"internet_max_bandwith_out"`
-	TemporaryKeyPairName     string        `mapstructure:"temporary_key_pair_name"`
-	WindowsPasswordTimeout   time.Duration `mapstructure:"windows_password_timeout"`
+	AssociatePublicIpAddress bool   `mapstructure:"associate_public_ip_address"`
+	ZoneId                   string `mapstructure:"zone_id"`
+	IOOptimized              bool   `mapstructure:"io_optimized"`
+	InstanceType             string `mapstructure:"instance_type"`
+	Description              string `mapstructure:"description"`
+	AlicloudSourceImage      string `mapstructure:"source_image"`
+	ForceStopInstance        bool   `mapstructure:"force_stop_instance"`
+	SecurityGroupId          string `mapstructure:"security_group_id"`
+	SecurityGroupName        string `mapstructure:"security_group_name"`
+	UserData                 string `mapstructure:"user_data"`
+	UserDataFile             string `mapstructure:"user_data_file"`
+	VpcId                    string `mapstructure:"vpc_id"`
+	VpcName                  string `mapstructure:"vpc_name"`
+	CidrBlock                string `mapstructure:"vpc_cidr_block"`
+	VSwitchId                string `mapstructure:"vswitch_id"`
+	VSwitchName              string `mapstructure:"vswitch_id"`
+	InstanceName             string `mapstructure:"instance_name"`
+	InternetChargeType       string `mapstructure:"internet_charge_type"`
+	InternetMaxBandwidthOut  int    `mapstructure:"internet_max_bandwith_out"`
+	TemporaryKeyPairName     string `mapstructure:"temporary_key_pair_name"`
 
 	// Communicator settings
 	Comm           communicator.Config `mapstructure:",squash"`

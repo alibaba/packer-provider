7c6140a6f28416f9dca9cdf16bbb3e1d6d857092
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2017-04-20 17:49:58 +0800

    add content that distinction os tyep

--- examples/alicloud/jenkins/jenkins.sh
+++ examples/alicloud/jenkins/jenkins.sh
@@ -8,10 +8,16 @@ TOMCAT_PACKAGE="$TOMCAT_NAME.tar.gz"
 TOMCAT_URL="http://mirror.bit.edu.cn/apache/tomcat/tomcat-7/v$TOMCAT_VERSION/bin/$TOMCAT_PACKAGE"
 TOMCAT_PATH="/opt/$TOMCAT_NAME"
 
-install jdk
-sudo apt-get update -y
-sudo apt-get install -y openjdk-7-jdk
-
+#install jdk
+if grep -Eqi "Ubuntu|Debian|Raspbian" /etc/issue || grep -Eq "Ubuntu|Debian|Raspbian" /etc/*-release; then
+        sudo apt-get update -y
+        sudo apt-get install -y openjdk-7-jdk
+elif grep -Eqi "CentOS|Fedora|Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "CentOS|Fedora|Red Hat Enterprise Linux Server" /etc/*-release; then
+        sudo yum update -y
+        sudo yum install -y openjdk-7-jdk
+else
+        echo "Unknown OS type."
+fi
 #install jenkins server
 mkdir ~/work
 cd ~/work
